122842abffea89063241e6fd4ef301c42ea4f8ee
yukikobayashi0602 <Yu.glanz1@au.com>
2025-11-29 15:23:22 +0900

    Add a command

---
+++ averagetest.bash
@@ -0,0 +1,17 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Kobayashi Yuki
+# SPDX-License-Identifier: BSD-3-Clause
+
+# テスト対象コマンド
+CMD="python3 avg.py"
+
+ng () {
+    echo "NG at Line $1"
+    res=1
+}
+
+res=0
+
+### 正の数字のみ ###
+out=$($CMD 1 2 3 4 5)
+[ "${out}" = "3.0" ] || ng ${LINENO}
