e9f4741edfea04bfe4db94e6abeb0a60825e107c
yukikobayashi0602 <Yu.glanz1@au.com>
2025-11-30 13:53:35 +0900

    Add a command

---
+++ standarddeviationtest.bash
@@ -0,0 +1,18 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Kobayashi Yuki
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+    echo NG at Line $1
+    res=1
+}
+
+res=0
+
+equal() {
+    [ "$1" = "$2" ]
+}
+
+### 正の数字のみ ###
+out=$(./standarddeviation 1 2 3 4)
+equal "${out}" "1.118033988749895" || ng ${LINENO}
