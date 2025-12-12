3ae3c067bbb953416b09bb2730974b10891f450c
yukikobayashiyashi <147679322+yukikobayashi0602@users.noreply.github.com>
2025-12-12 16:04:41 +0900

    Update test.bash

--- test.bash
+++ test.bash
@@ -16,8 +16,8 @@ out=$(seq 5 | ./plus)
 out=$(seq 5.5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
-out=$(./plus < numms)
-[ "${out}" = -2.2 ] || ng ${LINENO}
+out=$(./plus < nums)
+[ "${out}" = 6 ] || ng ${LINENO}
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
